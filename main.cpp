4fa79ad56718714024d49475b618f18a31ad7208
Elena Yajure <egyajure@gmail.com>
2021-08-28 15:09:16 -0600

    Altered goBack function which lead to improved dmoj results, now having no wrong answers but instead having runtime errors and time limits being exceeded for the larger inputs

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ struct Square
     void addCoordinate(int x, int y);
 };
 
-Square::Square(int a) : value{a}{
+Square::Square(int a) : value{a}
+{
     //contructor, intentionally empty
 }
 
@@ -60,7 +61,7 @@ void gatherData(std::vector<Square> &vect, int x, int y)
 }
 
 //moved stuff from 'lastLocation' here
-int count;
+int count = 0;
 std::vector<Square> moves;
 
 void keepTrack(Square f)
@@ -69,18 +70,34 @@ void keepTrack(Square f)
     moves.push_back(f);
 };
 
+//where I thinbk the mistake is
 void goBack(std::vector<Square> &vect, Square &f, int option)
 {
-    f = moves[0];
-    //want to delete the first element in the vector
-    for (int i = 0; i < count; i++)
+    f = moves[count - 1];
+    //want to delete the first WRONG WE WANT LAST element in the vector
+    /*std::cout<< "current moves: ";
+    for (int i = 0; i < moves.size(); i++)
     {
-        moves[i] = moves[i + 1];
-    }
+        Square c (0);
+        c = moves[i];
+        std::cout << "\n"<< c.value;
+        //moves[i] = moves[i + 1];
+    }*/
     moves.pop_back();
+    /*std::cout<< "new moves: ";
+    for (int i = 0; i < moves.size(); i++)
+    {
+        Square c (0);
+        c = moves[i];
+        std::cout << "\n"<< c.value;
+        //moves[i] = moves[i + 1];
+    }
+    */
     count--;
-    Square o(0);      //making a square with a value of zero
+   /* Square o(0);      //making a square with a value of zero
     vect[option] = o; //replacing current option with the square with the value of zero
+    no longer need this part because of the 'beenSeen' addition
+    */
 };
 
 //moved stuff from endcheck here
@@ -94,13 +111,12 @@ void endCheck(Square &f, std::string &output, int X, int Y, bool &end, bool opti
         //std::cout << "done \n";
     }
     //checking if the code is stuck where it started. indicating escape is not possible
-    if (f.xCoordinate == X && f.yCoordinate == Y && optionFound == false)
+    if (f.xCoordinate == X && f.yCoordinate == Y && /*NEW*/ moves.size() <= 1 && optionFound == false)
     {
         output = "no";
         end = true;
         //std::cout << "done \n";
     }
-    
 }
 
 //stuff from findOptions here
@@ -108,12 +124,14 @@ int findOptions(std::vector<Square> v, int X, int Y, bool &optionFound, Square s
 {
     //before goal was x * Y, only wokred for first 'move'
     int goal = s.xCoordinate * s.yCoordinate;
-    float I = 0;
+    float I = 0.5;
     optionFound = false;
 
-    for (int i = 0; i < v.size(); i++)
+    //std::cout<< "search start\n";
+    for (int i = 0; i <= v.size(); i++)
     {
         Square o = v[i];
+        //std::cout << o.value << "\n";
         if (o.value == goal /*new*/ && o.beenSeen == false)
         {
             I = i;
@@ -125,7 +143,8 @@ int findOptions(std::vector<Square> v, int X, int Y, bool &optionFound, Square s
 
 //*new* function to mark spot in vector as visited
 
-void markVisited (std::vector<Square> &vect, int spot){
+void markVisited(std::vector<Square> &vect, int spot)
+{
     Square a = vect[spot];
     a.beenSeen = true;
     vect[spot] = a;
@@ -146,17 +165,30 @@ int main()
     //running the modeule 'gatherData' to fill the vector created with values and coordinates
     gatherData(vect, X, Y);
 
+    //NEXT STEP WHEN COMING BACK FROM KUMON -> MAKE GATHERDATA TEST AND FIND WHY THE FIRST VALUE IS ALREADY ZERO
+    //gatherdata test
+    /*
+    for (int i = 0; i < vect.size(); i++)
+    {
+        Square b(0);
+        b = vect[i];
+        std::cout << b.value;
+    }
+    */
 
     Square f = vect[Y * X - 1]; //creating a special object, f, which will be the 'travelling' object
     //*new* marking the travelling object as seen before
-    f.beenSeen = true;
+    markVisited(vect, (Y * X - 1)); // FIX
+ 
 
-    bool optionFound = false;   // creating a bool that will be modified in the 'findoptions module'
+    bool optionFound = false; // creating a bool that will be modified in the 'findoptions module'
 
     bool end = false;
     float option = 0.5;
     std::string output = " ";
 
+    //std::cout << "start coordinate = " << f.xCoordinate << ", " << f.yCoordinate << "\n";
+    //std::cout << "start value = " << f.value << "\n";
 
     while (end == false)
     {
@@ -171,7 +203,7 @@ int main()
             f = vect[option];
 
             //*new* current option is now marked as visited
-            markVisited (vect, option);
+            markVisited(vect, option);
 
             //for keeping track of where the program is at
             //std::cout << "new coordinate = " << f.xCoordinate << ", " << f.yCoordinate << "\n";
@@ -185,10 +217,18 @@ int main()
 
             //changes the vector at this location to make the value 0, goes back a step
             goBack(vect, f, option);
+    
+            //std::cout << "new coordinate = " << f.xCoordinate << ", " << f.yCoordinate << "\n";
+            //std::cout << "new value = " << f.value << "\n";
         }
 
         //at the end of each loop, the code will check to see if it is done
+        //
+        //**MISTAKE, changes coordinates and then checks if we're done without looking for more options
+        //fix, checking options at the end of the loop as well
+        option = findOptions(vect, f.xCoordinate, f.yCoordinate, optionFound, f);
         endCheck(f, output, X, Y, end, optionFound);
+
         //std::cout << "endCheck = " << end << "\n";
     }
     //std::cout << "end of loop reached\n\n";
